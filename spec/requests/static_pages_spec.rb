b9d34dd3cc38fcf8447a142102a738ce7b9dd2e5
Amos Friling <amos.friling@teva.co.il>
2013-09-22 22:18:29 +0300

    Add contact us

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -39,5 +39,19 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
   end
+  
+  describe "Contact Us" do
+    
+    it "should have the right title" do
+    	    visit'/static_pages/contact'
+    	    expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact Us')
+    end
+    
+    it "should have the content 'Sample App'" do
+    	    visit '/static_pages/contact'
+      expect(page).to have_content('Sample App')
+    end
+  end
+  
 end
 
